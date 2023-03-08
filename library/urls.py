adf02ab31efea51a8d17856c711e64778fff85bd
Arman Mohammadi <rman.mo2000@gmail.com>
2023-03-08 16:42:48 +0330

    Create API for creating Author
    A CreateAPIView is created to handle creating objects of type Author.

--- library/urls.py
+++ library/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
+from .views import *
 
 urlpatterns = [
-    
+    path('author/crate', CreateAuthor.as_view()),
 ]
--- library/views.py
+++ library/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Author
+from .serializers import AuthorSerializer
 
-# Create your views here.
+class CreateAuthor(generics.CreateAPIView):
+    serializer_class = AuthorSerializer
+    queryset = Author.objects.all()
