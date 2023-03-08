06d405913bc59985fb739a140ed6ba6fc2f4bd37
Arman Mohammadi <rman.mo2000@gmail.com>
2023-03-08 16:39:35 +0330

    Create Book Serializer
    Serializer of book model has been created.

--- library/serializers.py
+++ library/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Author
+from .models import Author, Book
 
 class AuthorSerializer(ModelSerializer):
     class Meta:
         model = Author
         fields = ['first_name', 'last_name', 'birth_date', 'email']
-        
+
+
+class BookSerializer(ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['name', 'year', 'number_of_pages', 'edition', 'author']
