c04afceea3f1e2c6d6d3ea05dbde700073e16bca
Arman Mohammadi <rman.mo2000@gmail.com>
2023-03-08 16:30:24 +0330

    Create Author model
    Author model which contains information about authors of the book in the systems is created.

--- library/models.py
+++ library/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    birth_data = models.DateField()
+    created = models.DateTimeField(auto_now=True)
+    email = models.EmailField()
