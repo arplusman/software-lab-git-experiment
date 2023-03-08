a76e55b297bca1754fa7b0f622a7e1e3c3093227
Arman Mohammadi <rman.mo2000@gmail.com>
2023-03-08 16:45:03 +0330

    Create API for creating Book
    a CreateAPIView has been created to handle creating objects of type Book.

--- library/urls.py
+++ library/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('author/crate', CreateAuthor.as_view()),
+    path('author/crate/', CreateAuthor.as_view()),
+    path('book/create/', CreateBook.as_view()),
 ]
--- library/views.py
+++ library/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Author
-from .serializers import AuthorSerializer
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
 
 class CreateAuthor(generics.CreateAPIView):
     serializer_class = AuthorSerializer
     queryset = Author.objects.all()
+
+
+class CreateBook(generics.CreateAPIView):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+    
