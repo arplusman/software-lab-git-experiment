d6f3c0fed93c35c54ba5ddfcf756fde72f5de6a5
Arman Mohammadi <rman.mo2000@gmail.com>
2023-03-08 15:55:45 +0330

    Install Debug Toolbar
    Django debug toolbar has been isntalled in the system and all its configs has been added to appropriate files.

--- core/settings.py
+++ core/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
