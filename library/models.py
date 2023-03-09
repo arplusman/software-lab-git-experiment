8fcb5b1af699e38af1f33d8e5cfb0a93bc4ecdf0
Arman Mohammadi <rman.mo2000@gmail.com>
2023-03-09 16:07:43 +0330

    Correct birth_bate attribute
    birth_date was incorrectly spelled birth_data in models.py file in app library. It is refactored and corrected in this commit.

--- library/models.py
+++ library/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Author(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    birth_data = models.DateField()
+    birth_date = models.DateField()
     created = models.DateTimeField(auto_now=True)
     email = models.EmailField()
 
