d3a9ba340fe9e2f4f8b8c78e6f8def4bb76ab23e
Arman Mohammadi <rman.mo2000@gmail.com>
2023-03-09 16:19:09 +0330

    Conflicted file bug fix
    A line that refeting to HEAD was not deleted in conflict management.

--- library/models.py
+++ library/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Author(models.Model):
-<<<<<<< HEAD
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     birth_date = models.DateField()
