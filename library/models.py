d729db904d5a0201f73da1bf0a5bc5b6722a64ca
Arman Mohammadi <rman.mo2000@gmail.com>
2023-03-09 16:08:58 +0330

    Change max length
    Max length of first_name and last_name changed from 255 to 100.

--- library/models.py
+++ library/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Author(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     birth_date = models.DateField()
     created = models.DateTimeField(auto_now=True)
     email = models.EmailField()
